e0ff4e39287346036a81cdb51ba3adb2b0cffc6c
ljws93 <ljws93@naver.com>
2021-08-25 23:24:08 +0900

    built recyclerView for movieActivity

--- app/src/main/java/com/example/movie_mvvmclean_udemy/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/movie_mvvmclean_udemy/presentation/movie/MovieActivity.kt
@@ -3,9 +3,12 @@ package com.example.movie_mvvmclean_udemy.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.view.isGone
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movie_mvvmclean_udemy.R
 import com.example.movie_mvvmclean_udemy.databinding.ActivityMovieBinding
 import com.example.movie_mvvmclean_udemy.presentation.di.Injector
@@ -16,6 +19,7 @@ class MovieActivity : AppCompatActivity() {
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieVieModel : MovieViewModel
     private lateinit var binder : ActivityMovieBinding
+    private lateinit var adapter : MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binder = DataBindingUtil.setContentView(this, R.layout.activity_movie)
@@ -23,8 +27,28 @@ class MovieActivity : AppCompatActivity() {
             .inject(this)
 
         movieVieModel = ViewModelProvider(this, factory).get(MovieViewModel::class.java)
+        initRecyclerView()
+    }
+    private fun initRecyclerView(){
+        binder.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binder.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies(){
+        binder.movieProgressBar.isGone = false
         val responseLiveData = movieVieModel.getMovies()
         responseLiveData.observe(this, Observer {
+            if (it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binder.movieProgressBar.isGone = true
+            }else{
+                binder.movieProgressBar.isGone = true
+
+                Toast.makeText(applicationContext, "There is no data", Toast.LENGTH_SHORT).show()
+            }
             Log.d("test", it.toString())
         })
     }
